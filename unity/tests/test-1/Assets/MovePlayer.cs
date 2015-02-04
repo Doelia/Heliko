530e25dde6cf11573ca62057542bfcc22b507f87
Doelia <kyxalia@gmail.com>
2015-02-04 13:21:53 +0100

    ajout d'un test de déplacement

---
+++ unity/tests/test-1/Assets/MovePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	Vector3 velocity;
+	// Use this for initialization
+	void Start () {
+		this.velocity = new Vector3 (10, 0, 0);
+		Physics.gravity = new Vector3 (0, -100, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody.velocity = new Vector3(10, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+
+		if (Input.GetKeyDown ("space")) {
+			this.rigidbody.velocity = new Vector3(10, 30, this.rigidbody.velocity.z);
+		}
+
+	}
+}
