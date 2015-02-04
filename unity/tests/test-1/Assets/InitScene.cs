8a179516c5c71aeeca31f0c1e9137b2d6c61cc5b
unknown <znoraka@gmail.com>
2015-02-04 14:07:37 +0100

    ajout de plein d'obstacles (100)

---
+++ unity/tests/test-1/Assets/InitScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitScene : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform prefab;
+
+	void Start () {
+		for (int i = 0; i < 100; i++) {
+			Instantiate(prefab, new Vector3(i * 5, 1, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- unity/tests/test-1/Assets/MovePlayer.cs
+++ unity/tests/test-1/Assets/MovePlayer.cs
@@ -7,15 +7,15 @@ public class MovePlayer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.velocity = new Vector3 (10, 0, 0);
-		Physics.gravity = new Vector3 (0, -100, 0);
+		Physics.gravity = new Vector3 (0, -500, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.rigidbody.velocity = new Vector3(10, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+		this.rigidbody.velocity = new Vector3(15, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
 
 		if (Input.GetKeyDown ("space")) {
-			this.rigidbody.velocity = new Vector3(10, 30, this.rigidbody.velocity.z);
+			this.rigidbody.velocity = new Vector3(15, 70, this.rigidbody.velocity.z);
 		}
 
 	}
