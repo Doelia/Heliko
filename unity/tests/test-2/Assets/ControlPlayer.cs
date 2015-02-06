ccc5f4051f15d1ac94f3f50eee4ac0798ec46177
Doelia <kyxalia@gmail.com>
2015-02-06 19:54:15 +0100

    ajout d'un système de gravité autour d'une planète

---
+++ unity/tests/test-2/Assets/ControlPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPlayer : MonoBehaviour {
+
+	public Transform planet;
+	public int forceJump = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void jump() {
+		Vector3 direction = this.planet.position - this.transform.position;
+		direction = - direction.normalized;
+		this.rigidbody.velocity = direction * forceJump;
+	}
+
+	void move() {
+		Vector3 vec = this.planet.position - this.transform.position;
+		vec = Quaternion.AngleAxis(90, Vector3.up) * vec;
+		this.rigidbody.velocity = vec;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			this.jump ();
+		} else {
+			this.move ();
+		}
+	}
+}
