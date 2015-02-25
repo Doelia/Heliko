f167b20c1697cb1bb92ae64e104641262564a3ce
unknown <znoraka@gmail.com>
2015-02-25 14:56:10 +0100

    deplacement fichiers

---
+++ unity/tests/test-6-moteur/Assets/scripts/TextTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTest : MonoBehaviour {
+
+	public TextAsset txtFile;
+	public string [] fileData;
+
+	// Use this for initialization
+	void Start () {
+		fileData = txtFile.text.Split ('\n');
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (fileData[0]);
+	}
+}
