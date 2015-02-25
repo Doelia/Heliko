c4de056433f2f01773e05edadc97232e842dcaac
unknown <znoraka@gmail.com>
2015-02-25 15:27:55 +0100

    lecture d'un level

--- unity/tests/test-6-moteur/Assets/scripts/LevelScriptedNotifier.cs
+++ unity/tests/test-6-moteur/Assets/scripts/LevelScriptedNotifier.cs
@@ -6,10 +6,29 @@ using System.Collections;
  * 
  */
 
-
 public class LevelScriptedNotifier : TempoReceiver {
 
-	public void notifChilds(int type) {
+	public TextAsset levelData;
+	private int[] stepEvents;
+	private int[] halfStepsEvents;
+	private int eventIndex;
+
+	public void loadData() {
+		string [] tracks = levelData.text.Split ('\n');
+		stepEvents = stringToIntEvents (tracks [0]);
+		halfStepsEvents = stringToIntEvents (tracks [1]);
+	}
+
+	private int [] stringToIntEvents(string s) {
+		string [] events = s.Split (' ');
+		int [] toReturn = new int[events.Length];
+		for (int i = 0; i < events.Length; i++) {
+			toReturn [i] = int.Parse (events [i]);
+		}
+		return toReturn;
+	}
+
+	private void notifyChildren(int type) {
 		foreach (Transform s1 in transform) {
 			if (s1.GetComponent<LevelScriptedReceiver>() != null) {
 				s1.GetComponent<LevelScriptedReceiver>().onEventType(type);
@@ -21,10 +40,10 @@ public class LevelScriptedNotifier : TempoReceiver {
 	}
 	                                
 	public override void onStep() {
-
+		notifyChildren (stepEvents [eventIndex++]);
 	}
 
 	public override void onHalfStep() {
-
+		notifyChildren (stepEvents [eventIndex]);
 	}
 }
