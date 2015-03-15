23688b96270e911fd7fbf35b55b54ed73c285b48
Doelia <kyxalia@gmail.com>
2015-03-15 18:45:15 +0100

    Amélioration du mini jeu

--- unity/tests/test-8-phoques-v2/Assets/Level/Scripts/IAModel.cs
+++ unity/tests/test-8-phoques-v2/Assets/Level/Scripts/IAModel.cs
@@ -5,21 +5,14 @@ public class IAModel : MonoBehaviour, LevelScriptedReceiver {
 
 	private Animator anim;
 	public LevelScripted level;
-	public AudioSource snare;
 
 	public void Start () {
 		this.level.connect(this);
 		anim = GetComponent < Animator >();
 	}
 	
-	void Update () {
-		
-	}
-
 	public void onEventType (int type) {
-		Debug.Log("Event type "+type);
 		if (type == 1) {
-			snare.Play();
 			anim.SetTrigger ("change");
 		}
 	}
