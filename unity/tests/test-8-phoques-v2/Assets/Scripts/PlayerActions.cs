04a15445172cfd5e03191c399771512303f2beed
unknown <znoraka@gmail.com>
2015-03-17 11:08:27 +0100

    ajout gestion score

--- unity/tests/test-8-phoques-v2/Assets/Scripts/PlayerActions.cs
+++ unity/tests/test-8-phoques-v2/Assets/Scripts/PlayerActions.cs
@@ -5,10 +5,14 @@ public class PlayerActions : MonoBehaviour {
 
 	public LevelScripted level;
 	private ArrayList successStep;
+	private int failuresCount;
+	private int stepsCount;
 
 	// Use this for initialization
 	void Start () {
 		this.successStep = new ArrayList();
+		failuresCount = 0;
+		stepsCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -22,10 +26,12 @@ public class PlayerActions : MonoBehaviour {
 			this.successStep.Add(stepTapped);
 			return true;
 		}
+		failuresCount++;
 		return false;
 	}
 
 	public void onAction(int action){
+		stepsCount++;
 		int nStep = level.beatCounter.getNBeat();
 		if (nStep > 1) {
 			int previousStep = nStep-1;
@@ -33,5 +39,13 @@ public class PlayerActions : MonoBehaviour {
 
 			}
 		}
+	}
+
+	public int getNumberOfSuccess() {
+		return successStep.Count;
+	}
+
+	public int getScore() {
+		return stepsCount - failuresCount;
 	}
 }
