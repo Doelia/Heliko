b3b700cd3a49f69c17d0f297df528a81a7e13e34
Doelia <kyxalia@gmail.com>
2015-03-18 10:20:10 +0100

    Update level

--- unity/tests/test-10-champi/Assets/Level/Scripts/IAModel.cs
+++ unity/tests/test-10-champi/Assets/Level/Scripts/IAModel.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 public class IAModel : MonoBehaviour, LevelScriptedReceiver {
 
-	private Animator anim;
+	private Animator brasDroit;
+	private Animator brasGauche;
 	public LevelScripted level;
 
-	public Transform animationTransform;
+	public Transform brasGaucheTrasform;
+	public Transform brasDroitTrasform;
 	public Transform carapace;
 
 	public void Start () {
 		this.level.connect(this);
-		anim = animationTransform.GetComponent<Animator>();
+		brasDroit = brasDroitTrasform.GetComponent<Animator>();
+		brasGauche = brasGaucheTrasform.GetComponent<Animator>();
 	}
 	
 	public void onAction (int type) {
 		if (type == 1) {
-			anim.SetTrigger ("Move");
+			brasDroit.SetTrigger ("Move");
+			this.GetComponent<AudioSource>().Play();
+			carapace.GetComponent<Animator>().SetTrigger("Move");
+		} else {
+			brasGauche.SetTrigger ("Move");
 			this.GetComponent<AudioSource>().Play();
 			carapace.GetComponent<Animator>().SetTrigger("Move");
 		}
