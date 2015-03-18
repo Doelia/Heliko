8a8fd411d2e13e0ec51109f97534b9f3e1524ba7
Doelia <kyxalia@gmail.com>
2015-03-18 10:45:06 +0100

    Level OK

---
+++ unity/tests/test-10-champi/Assets/ChampiFan.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChampiFan : MonoBehaviour,LevelScriptedReceiver  {
+
+	private Animator anim;
+	public LevelScripted level;
+	
+	public void Start () {
+		this.level.connect(this);
+		anim = this.GetComponent<Animator>();
+	}
+	
+	public void onAction (int type) {
+		if (type == 1) {
+			anim.SetTrigger ("Move");
+		}
+	}
+	
+	public void onFailure() {
+		
+	}
+
+}
