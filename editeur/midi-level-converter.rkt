59417713bc57977ddf4040596bcff25a3d2f8235
znoraka <znoraka@gmail.com>
2015-03-20 11:41:18 +0100

    avancement dans le convertisseur de niveau

--- editeur/midi-level-converter.rkt
+++ editeur/midi-level-converter.rkt
@@ -8,12 +8,12 @@
   (let ([division (header-division (car midi-data))]
         [delta-time (time-signature-cc (car (track-event-event (track-chunk-events (cadr midi-data)))))])    
     (to-level (car (filter (λ (i) (not (empty? i)))
-                 (foldr (λ (i l)
-                          (if (track-chunk? i)
-                              (cons (foldr (λ (i l) 
-                                             (if (midi-event? i)
-                                                 (cons i l) l)) '() (track-event-event (track-chunk-events i))) l)
-                              l)) '() midi-data))) division delta-time)))
+                           (foldr (λ (i l)
+                                    (if (track-chunk? i)
+                                        (cons (foldr (λ (i l) 
+                                                       (if (midi-event? i)
+                                                           (cons i l) l)) '() (track-event-event (track-chunk-events i))) l)
+                                        l)) '() midi-data))) division delta-time)))
 
 (define (event-off? event)
   (and (> (midi-event-instruction event) 127)
@@ -23,29 +23,42 @@
   (and (> (midi-event-instruction event) 143)
        (< (midi-event-instruction event) 160)))
 
+(define (vlq->int l)
+  (let f ([i 0] [l l] [sum 0])
+    (if (empty? l)
+        sum
+        (f (+ 1 i) (cdr l) (+ sum (* (if (> (car l) 127) (- (car l) 128) (car l)) (expt 128 i)))))))
+
+(define (transpose lists)
+  (let f ([l lists] [data '()])
+    (if (empty? (car l))
+        data
+        (f (map cdr l) (append data `(,(map (λ (i)
+                                              (car i)) l)))))))
+
 (define (event-to-level event delta-time)
-  (append (make-list (/ (to-int (midi-event-delta event)) delta-time) 0) '(1)))
+  (append (make-list (/ (vlq->int (midi-event-delta event)) delta-time) 0) '(1)))
+
+(define (export level out)
+  (for-each (λ (i)
+         (for-each (λ (i)
+                (write i out)) i) (write-char #\newline out)) level)
+  (close-output-port out))
 
 (define (to-level events division delta-time)
-  (pretty-display events)
   (let f ([level '()] [delta-sum 0] [events events])
-    (displayln level)
     (if (empty? events)
-        (foldr (λ (i l)
-                 (if (= (length (car l)) (/ division delta-time))
-                     (cons `(,i) l)
-                     (append (append (car l) `(,i)) (cdr l)))) '() level)
+        (transpose (filter (λ (i)
+                             (= (length i) (/ division delta-time))) (reverse (foldl (λ (i l)
+                          (cond[(= (length (car l)) (/ division delta-time)) (cons `(,i) l)]
+                               [else (append `(,(append (car l) `(,i))) (cdr l))])) '(()) level))))
         (if (event-on? (car events))
             (f (append level (event-to-level (car events) delta-time)) 0 (cdr events))
             (if (event-off? (car events))
-              (f level (+ delta-time (to-int (midi-event-delta (car events)))) (cdr events))
-               (f level delta-time (cdr events)))))))
-            
-
-
-
+                (f (append level '(0)) (+ delta-time (to-int (midi-event-delta (car events)))) (cdr events))
+                (f level delta-time (cdr events)))))))
 
 (define (main)
-  (convert (parse-midi-file (open-input-file "./test.mid" #:mode 'binary))))
+  (export (convert (parse-midi-file (open-input-file "./test.mid" #:mode 'binary))) (open-output-file "out.txt" #:mode 'binary #:exists 'replace)))
 
 (main)
