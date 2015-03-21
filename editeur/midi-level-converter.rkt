bd6620c9f15b5c875119f04e91c772508a452d87
noe <noe.lephilippe@gmail.com>
2015-03-21 19:44:56 +0100

    gestion des différentes notes

--- editeur/midi-level-converter.rkt
+++ editeur/midi-level-converter.rkt
@@ -4,10 +4,15 @@
 
 (define one 60)
 (define notes (hash 60 1
-                    62 2
-                    64 3))
-                    
-                        
+                    61 2
+                    62 3
+                    63 4
+                    72 1
+                    73 2
+                    74 3
+                    75 4))
+
+
 (define (convert midi-data)
   (let ([division (header-division (car midi-data))]
         [delta-time (time-signature-cc (car (track-event-event (track-chunk-events (cadr midi-data)))))])    
@@ -36,25 +41,27 @@
 (define (transpose matrix)
   (if (empty? (car matrix))
       '()
-    (cons (map (λ (i)
-         (car i)) matrix) (transpose (map cdr matrix)))))
+      (cons (map (λ (i)
+                   (car i)) matrix) (transpose (map cdr matrix)))))
 
 (define (event-to-level event delta-time)
-  (append (make-list (/ (vlq->int (midi-event-delta event)) delta-time) 0) `(,(hash-ref notes (midi-event-arg1 event) 0))))
+  (append (make-list (/ (vlq->int (midi-event-delta event)) delta-time) 0) `(,(hash-ref notes (midi-event-arg1 event) #\?))))
 
 (define (export level out)
   (for-each (λ (i)
-         (for-each (λ (i)
-                (write i out)) i) (write-char #\newline out)) level)
+              (for-each (λ (i)
+                          (write i out)) i) (write-char #\newline out)) level)
   (close-output-port out))
 
 (define (to-level events division delta-time)
   (let f ([level '()] [delta-sum 0] [events events])
     (if (empty? events)
         (transpose (filter (λ (i)
-                             (= (length i) (/ division delta-time))) (reverse (foldl (λ (i l)
-                          (cond[(= (length (car l)) (/ division delta-time)) (cons `(,i) l)]
-                               [else (append `(,(append (car l) `(,i))) (cdr l))])) '(()) level))))
+                             (= (length i) (/ division delta-time)))
+                           (reverse
+                            (foldl (λ (i l)
+                                     (cond[(= (length (car l)) (/ division delta-time)) (cons `(,i) l)]
+                                          [else (append `(,(append (car l) `(,i))) (cdr l))])) '(()) level))))
         (if (event-on? (car events))
             (f (append level (event-to-level (car events) delta-time)) 0 (cdr events))
             (if (event-off? (car events))
