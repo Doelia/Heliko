d33bd7f1f802bf56e0524a63e57a734e204bc37f
robinDesBits <jolan.konig@gmail.com>
2015-03-25 15:27:42 +0100

    Tactile
    Ajout du tactile, 4 fonction actions sur le cube qui sont appelé en
    fonction de l'évènement détecté. Les évènements sont ceux sur le trello,
    je peux ajouter modifier sans aucun problème et rapidement

---
+++ unity/tests/test-12-tactile/Assets/cube.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void action1()
+	{
+		if (this.transform.localScale != Vector3.one * 3) {
+						this.transform.localScale = Vector3.one * 3;
+				} else {
+			this.transform.localScale = Vector3.one;
+
+		}
+	}
+	public void action2()
+	{
+		if (this.transform.localScale != Vector3.one / 3) {
+			this.transform.localScale = Vector3.one / 3;
+		} else {
+			this.transform.localScale = Vector3.one;
+			
+		}	}
+	public void action3()
+	{
+		if (this.transform.position != new Vector3 (1, 0, 0)) {
+						this.transform.position = new Vector3 (1, 0, 0);
+				} else {
+				this.transform.position = new Vector3 (0, 0, 0);
+
+				}
+		
+	}
+	public void action4()
+	{
+		if (this.transform.position != new Vector3 (-1, 0, 0)) {
+			this.transform.position = new Vector3 (-1, 0, 0);
+		} else {
+			this.transform.position = new Vector3 (0, 0, 0);
+			
+		}
+	}
+}
---
+++ unity/tests/test-12-tactile/Assets/tactilEvent.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class tactilEvent : MonoBehaviour {
+
+	public float timeBeforeLongTouch;
+	public cube monCube;
+
+	private bool touchScreen;
+	private Vector2 mouvement;
+	private float timeTouchTotal;
+
+
+	void Start()
+	{
+		touchScreen = false;
+		timeTouchTotal = 0F;
+		mouvement = Vector2.zero;
+	}
+
+
+	void Update () 
+	{
+		if (Input.touchCount > 0) 
+		{           
+			switch (Input.GetTouch(0).phase) 
+			{           
+				case TouchPhase.Began:
+					touchScreen=true;
+					break;  
+
+				case TouchPhase.Ended:
+				if(mouvement.magnitude>=4)
+				{
+					monCube.action4();
+				}
+				else if(timeTouchTotal>=timeBeforeLongTouch)
+				{
+					monCube.action3();
+				}
+				else
+				{
+					monCube.action1();
+				}
+				timeTouchTotal = 0F;
+				touchScreen=false; 
+				mouvement=Vector2.zero;
+					break;   
+
+				case TouchPhase.Moved: 
+				mouvement+=Input.GetTouch(0).deltaPosition;	
+
+					break;                
+			}  		
+		}
+		if (touchScreen) {
+			timeTouchTotal+=Time.deltaTime;
+			if(timeTouchTotal>=timeBeforeLongTouch)
+			{
+				monCube.action2();
+				touchScreen=false;
+			}
+		}
+	}
+	
+	void StartAction(TouchPhase p) 
+	{  
+				
+	}
+}
