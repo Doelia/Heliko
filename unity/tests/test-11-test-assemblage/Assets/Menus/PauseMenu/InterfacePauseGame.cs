7376a07c07047ca1b456be8cd35e8c34c3461ed3
Doelia <kyxalia@gmail.com>
2015-03-25 18:15:47 +0100

    Avancement menu

--- unity/tests/test-11-test-assemblage/Assets/Menus/PauseMenu/InterfacePauseGame.cs
+++ unity/tests/test-11-test-assemblage/Assets/Menus/PauseMenu/InterfacePauseGame.cs
@@ -6,6 +6,9 @@ public class InterfacePauseGame : MonoBehaviour {
 	public GameObject loadingScreen;
 	public GameObject pauseMenu;
 
+	public GameObject pauseIcon;
+	public GameObject playIcon;
+
 	public AudioSource music;
 
 	public void Reload() {
@@ -16,10 +19,14 @@ public class InterfacePauseGame : MonoBehaviour {
 	public void UnPause() {
 		pauseMenu.SetActive(false);
 		music.Play();
+		playIcon.SetActive(false);
+		pauseIcon.SetActive(true);
 	}
 
 	public void Pause() {
 		music.Pause ();
 		pauseMenu.SetActive(true);
+		playIcon.SetActive(true);
+		pauseIcon.SetActive(false);
 	}
 }
