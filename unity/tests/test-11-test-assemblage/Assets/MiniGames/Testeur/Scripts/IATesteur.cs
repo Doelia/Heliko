075ba8a5f5505829297ca46ca80fc38b7e0f48e1
Doelia <kyxalia@gmail.com>
2015-03-28 14:59:36 +0100

    Ajout scène de test

---
+++ unity/tests/test-11-test-assemblage/Assets/MiniGames/Testeur/Scripts/IATesteur.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IATesteur : MonoBehaviour, LevelScriptedReceiver {
+
+	private Animator anim;
+	public LevelScripted level;
+
+	public void Start () {
+		this.level.connect(this);
+		anim = GetComponent < Animator >();
+	}
+	
+	public void onAction (int type) {
+		if (type == 1) {
+			Debug.Log ("change");
+			anim.SetTrigger ("change");
+		}
+	}
+
+
+}
---
+++ unity/tests/test-11-test-assemblage/Assets/MiniGames/Testeur/Scripts/PlayerTesteur.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTesteur : MonoBehaviour, PlayerEventReceiver, PlayerActionReceiver {
+
+	public PlayerEventListener playerEventListener;
+	public PlayerActions playerActions;
+	public AudioSource clap;
+
+	void Start () {
+		playerEventListener.connect (this);
+		playerActions.connect (this);
+	}
+
+	public void changeColor(bool isGood) {
+		this.GetComponent<SpriteRenderer>().color = isGood ? new Color(0.7f,1,0.7f) : new Color(1,.7f,.7f);
+	}
+
+	public void onFailure() {
+		this.changeColor(false);
+	}
+
+	public void onFinger (int type) {
+		clap.Play();
+		this.changeColor(playerActions.isGood(type));
+	}
+
+}
