8ef75f857379d3a1e8d475b5183e63bf21b7cf0d
doelia <kyxalia@gmail.com>
2015-03-29 19:58:04 +0200

    Ajout sons

---
+++ unity/tests/test-11-test-assemblage/Assets/PlayerAnanas.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnanas : MonoBehaviour, PlayerEventReceiver, PlayerActionReceiver  {
+
+	public AnanasManager parentAnanas;
+
+	public PlayerEventListener playerEventListener;
+	public PlayerActions playerActions;
+
+	void Start () {
+		playerEventListener.connect (this);
+		playerActions.connect (this);
+	}
+
+	public void onFailure() {
+
+	}
+
+	public void onFinger (int type) {
+		if (type == 1) {
+			parentAnanas.removeLast();
+		}
+	}
+
+}
