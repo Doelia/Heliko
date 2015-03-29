eb9728c976a2989095a1d1a025fbddc3128e2a0e
doelia <kyxalia@gmail.com>
2015-03-29 17:18:47 +0200

    Animations

---
+++ unity/tests/test-14-dino/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Animator anim1;
+	public Animator anim2;
+	public Animator anim3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			anim1.SetTrigger("go");
+			anim2.SetTrigger("go");
+			anim3.SetTrigger("go");
+		}
+	}
+}
