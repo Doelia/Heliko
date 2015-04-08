a04c54e7d4e304f715226f35f873a4b56ad8c3c7
robinDesBits <jolan.konig@gmail.com>
2015-04-08 14:42:05 +0200

    ajout du bouton reinitialiser

---
+++ unity/project/Assets/Menus/MiniGamesMenu/reinitialiserJeu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reinitialiserJeu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Reinitialiser () {
+		PlayerPrefs.DeleteAll ();
+		Application.LoadLevel (0);
+	}
+	
+
+}
