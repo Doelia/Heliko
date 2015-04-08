343a3e280e4ca68bd6e7d178601c9d4e71f6f170
robinDesBits <jolan.konig@gmail.com>
2015-04-08 16:21:47 +0200

    Envoie scene google annatytic

---
+++ unity/project/Assets/Communs/GoogleAnalyticsReportScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoogleAnalyticsReportScene : MonoBehaviour {
+	
+	public GoogleAnalyticsV3 googleAnalytics;
+	public string nomScene;
+	// Use this for initialization
+	void Start () {
+	googleAnalytics.LogScreen(nomScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
