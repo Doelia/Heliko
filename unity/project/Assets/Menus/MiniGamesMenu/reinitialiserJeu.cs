37f33b33ca700274059561a652e9bdc9ab8d7c50
robinDesBits <jolan.konig@gmail.com>
2015-04-08 16:07:15 +0200

    ajout pluggin Google Analytic

--- unity/project/Assets/Menus/MiniGamesMenu/reinitialiserJeu.cs
+++ unity/project/Assets/Menus/MiniGamesMenu/reinitialiserJeu.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class reinitialiserJeu : MonoBehaviour {
+public class ReinitialiserJeu : MonoBehaviour {
 
 	// Use this for initialization
 	public void Reinitialiser () {
