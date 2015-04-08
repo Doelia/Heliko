f8a69461fa3a9e14dbcfaf512fd86b0edbb1914f
robinDesBits <jolan.konig@gmail.com>
2015-04-08 13:28:46 +0200

    Affichage du bon nombre d'étoile en fonction de la reussite

---
+++ unity/project/Assets/Menus/MiniGamesMenu/etoilesMenuMiniGames.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class etoilesMenuMiniGames : MonoBehaviour {
+
+	private int nbrEtoiles;
+	public int idMiniGame;
+	// Use this for initialization
+	void Start () {
+		nbrEtoiles=PlayerPrefs.GetInt("etoileLevel"+idMiniGame,0);
+		for(int i=nbrEtoiles+1;i<=3;i++)
+		{
+			GameObject.Find ("s"+i).GetComponent<Image>().enabled=false;
+		}
+	}
+	
+
+}
