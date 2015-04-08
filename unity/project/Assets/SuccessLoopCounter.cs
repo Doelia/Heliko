4a799318ff1ae6ed86d0e4a28c735d2e48cfc493
Doelia <kyxalia@gmail.com>
2015-04-08 10:41:16 +0200

    Compteur de réussite OK

---
+++ unity/project/Assets/SuccessLoopCounter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SuccessLoopCounter : MonoBehaviour {
+
+	int max = 3;
+	int nbrSuccess = 0;
+
+	public void Start() {
+		this.Reset(3);
+		this.Hide ();
+	}
+
+	public void Reset(int max) {
+		this.nbrSuccess = 0;
+		this.max = max;
+		updateDisplay();
+	}
+
+	public bool AllSuccess() {
+		return (nbrSuccess >= max);
+	}
+
+	private void updateDisplay() {
+		this.GetComponent<Text>().text = nbrSuccess+"/"+max;
+	}
+
+	public void AddSuccess() {
+		this.nbrSuccess++;
+		updateDisplay();
+	}
+
+	public void Hide() {
+		this.gameObject.SetActive(false);
+	}
+
+	public void Show() {
+		this.gameObject.SetActive(true);
+	} 
+}
