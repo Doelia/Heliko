76b5aaec4bf4453593efd0f88f7e20b19dff9c7c
Doelia <kyxalia@gmail.com>
2015-04-29 17:43:32 +0200

    Ajout de la gestion du switch des objets

---
+++ unity/project/Assets/MagicObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicObject : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	public void ChangeObject(int n) {
+		Sprite sp;
+		sp = sprites[n+1];
+		this.GetComponent<SpriteRenderer>().sprite = sp;
+	}
+}
