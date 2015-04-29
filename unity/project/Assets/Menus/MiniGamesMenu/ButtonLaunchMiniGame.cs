379ca9e2eb08c309cec48df725c7c1acfa813e1b
Doelia <kyxalia@gmail.com>
2015-04-29 16:09:24 +0200

    Ajout script activer/desactiver bouton

--- unity/project/Assets/Menus/MiniGamesMenu/ButtonLaunchMiniGame.cs
+++ unity/project/Assets/Menus/MiniGamesMenu/ButtonLaunchMiniGame.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ButtonLaunchMiniGame : MonoBehaviour {
+
+	public Sprite enable;
+	public Sprite disable;
 
 	public void click() {
 		int idMiniGame = this.transform.parent.GetComponent<IdMiniGame>().id;
 		GameObject.Find ("Mini jeux").GetComponent<MiniGameLoader>().loadMiniGame(idMiniGame);
 	}
+
+	public void Start() {
+		if (true) {
+			this.GetComponent<Image>().sprite = enable;
+		} else {
+			this.GetComponent<Image>().sprite = disable;
+		}
+	}
+
+
 
 }
