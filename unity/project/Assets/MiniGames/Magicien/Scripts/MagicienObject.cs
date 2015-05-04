c52f934d301743239a1ac46c497d18c1f6c0d099
Thibaut C <thibaut.castanie@gmail.com>
2015-05-04 12:24:14 +0200

    Rythme tête magicien

--- unity/project/Assets/MiniGames/Magicien/Scripts/MagicienObject.cs
+++ unity/project/Assets/MiniGames/Magicien/Scripts/MagicienObject.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MagicienObject :  Feedback {
+public class MagicienObject :  Feedback, LevelScriptedReceiver {
 
 	private Animator anim;
+	public LevelScripted level;
 	
 	public Sprite content;
 	public Sprite pasContent;
 	
 	public new void Start () {
 		base.Start ();
+		this.level.connect(this);
 		anim = this.GetComponent<Animator>();
+	}
+
+	public void OnAction (int type) {
+		if (type == 1) {
+			anim.SetTrigger ("Move");
+		}
 	}
 	
 	public override void SetReaction(bool isGood) {
