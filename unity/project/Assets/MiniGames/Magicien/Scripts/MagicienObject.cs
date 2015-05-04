37d9eaaafb568a9077acc9421990cc9ed5d60e5a
Doelia <kyxalia@gmail.com>
2015-05-04 10:43:12 +0200

    Feedback magicien

---
+++ unity/project/Assets/MiniGames/Magicien/Scripts/MagicienObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicienObject :  Feedback {
+
+	private Animator anim;
+	
+	public Sprite content;
+	public Sprite pasContent;
+	
+	public new void Start () {
+		base.Start ();
+		anim = this.GetComponent<Animator>();
+	}
+	
+	public override void SetReaction(bool isGood) {
+		this.GetComponent<SpriteRenderer>().sprite = isGood?content:pasContent;
+	}
+
+}
