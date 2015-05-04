19c13c8e6a07051f431a5419ba53506f9abcef6b
Doelia <kyxalia@gmail.com>
2015-05-04 12:52:29 +0200

    Branchement de la tête avec le rythme

--- unity/project/Assets/MiniGames/Magicien/Scripts/MagicienObject.cs
+++ unity/project/Assets/MiniGames/Magicien/Scripts/MagicienObject.cs
@@ -7,14 +7,19 @@ public class MagicienObject :  Feedback, LevelScriptedReceiver {
 	
 	public Sprite content;
 	public Sprite pasContent;
+
+	private Animator anim;
 	
 	public new void Start () {
 		base.Start ();
 		this.level.connect(this);
+		anim = this.GetComponent<Animator>();
 	}
 
 	public void OnAction (int type) {
-
+		if (type == 1) {
+			anim.SetTrigger ("Move");
+		}
 	}
 	
 	public override void SetReaction(bool isGood) {
