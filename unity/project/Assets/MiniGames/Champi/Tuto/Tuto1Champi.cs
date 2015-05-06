131d2cd0af9d069fd5d5e68661074408ffff9b87
Doelia <kyxalia@gmail.com>
2015-05-06 10:53:12 +0200

    Avancement tuto champi

--- unity/project/Assets/MiniGames/Champi/Tuto/Tuto1Champi.cs
+++ unity/project/Assets/MiniGames/Champi/Tuto/Tuto1Champi.cs
@@ -11,19 +11,25 @@ public class Tuto1Champi : StepTuto, PlayerEventReceiver, PlayerActionReceiver {
 	public new void Start() {
 		if (isStart) return;
 		base.Start ();
-		GetPlayerActions().Connect (this);
 	}
 
 	public override void play () {
+		Debug.Log ("Start playing Tuto1Champi");
 		successLoopCounter.Reset (3);
 		successLoopCounter.Show ();
 
 		levelIA.connect(GameObject.Find("IA").GetComponent<PNJ>());
 
+		// Pour que le playerAction connaise le niveau à vérifier
 		GetPlayerActions().level = levelPlayer;
+		// Pour que le playerAction reçoive les évents du niveau
 		GetPlayerActions().level.connect (GetPlayerActions());
-
+		// Pour recevoir les évents du joueur
 		GetPlayerEventListener().connect(this);
+		// Pour recevoir la réussite et les echec du joueurs
+		GetPlayerActions().Connect (this);
+
+		// COmmencer à lire la musique
 		GetBeatCounter().StartCount();
 	}
 
