3ab26caf1bb92ac81f7ce6e34f5bd85771f4f3bb
unknown <znoraka@gmail.com>
2015-05-06 16:06:26 +0200

    intergation moteur

--- unity/project/Assets/MiniGames/Escargot/Scripts/MoveScript.cs
+++ unity/project/Assets/MiniGames/Escargot/Scripts/MoveScript.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class MoveScript : MonoBehaviour {
 
 	public Parallax [] parallax;
-	public AudioSource sound;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +15,9 @@ public class MoveScript : MonoBehaviour {
 	
 	}
 
-	public void avancer(int n = 5) {
+	public void avancer(float n) {
 		foreach (Parallax p in parallax) {
 			p.Move(n);
 		}
-		sound.Play ();
 	}
 }
---
+++ unity/project/Assets/MiniGames/Escargot/Scripts/PlayerRyan.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRyan : HelikoObject, PlayerEventReceiver {
+	
+	public AudioSource soundGood;
+	public AudioSource soundBad;
+	
+	public Transform ryan;
+
+	public MoveScript moveScript;
+
+	public float amplitude;
+	
+	private PlayerActions playerActions;
+	private bool leftPlayed;
+	
+	public new void Start () {
+		base.Start();
+		playerActions = GetPlayerActions();
+		
+		GetPlayerEventListener().connect (this);
+	}
+
+	private void moveRyan() {
+		foreach(Transform a in ryan) {
+			foreach(Transform t in a) {
+				t.GetComponent<Animator>().SetTrigger("Move");
+			}
+		}
+	}
+	
+	public void OnFinger (int type) {
+		moveRyan();
+		moveScript.avancer(amplitude);
+	}
+	
+}
