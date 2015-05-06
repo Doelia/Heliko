88d1e1cfc32b94b456f2754d32920954d3b5272a
robinDesBits <jolan.konig@gmail.com>
2015-05-06 17:40:35 +0200

    boutique Initialisation

---
+++ unity/project/Assets/shop/initialisationBoutique.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Soomla;
+using Soomla.Store;
+using Soomla.Store.maBoutique;
+public class initialisationBoutique : MonoBehaviour {
+
+	public static bool alreadyInitialized=false;
+	// Use this for initialization
+	void Start () {
+			if(alreadyInitialized)
+			{
+				GameObject.Destroy(this.gameObject);
+			}
+			else
+			{
+				SoomlaStore.Initialize(new boutique());
+				DontDestroyOnLoad(gameObject);
+				alreadyInitialized=true;
+			}
+
+
+	}
+	
+
+}
