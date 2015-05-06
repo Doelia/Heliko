fcc79102a231fc1783790141439459bb0b72ee02
Doelia <kyxalia@gmail.com>
2015-05-06 15:40:03 +0200

    Avancement tuto (step 2)

---
+++ unity/project/Assets/MiniGames/Champi/Tuto/Tuto2Champi.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+
+public class Tuto2Champi : StepTuto, PlayerEventReceiver, PlayerActionReceiver {
+
+	public LevelScripted levelPlayer;
+	public LevelScripted levelIA;
+
+	public SuccessLoopCounter successLoopCounter;
+	public AudioSource successStep;
+	public AudioSource successLoop;
+
+	public new void Start() {
+		if (isStart) return;
+		base.Start ();
+	}
+
+	public override void play () {
+		Debug.Log ("Start playing Tuto2Champi");
+		successLoopCounter.Reset (3);
+		successLoopCounter.Show ();
+
+		levelIA.connect(GameObject.Find("IA").GetComponent<PNJ>());
+
+		// Pour que le playerAction connaise le niveau à vérifier
+		GetPlayerActions().level = levelPlayer;
+		// Pour que le playerAction reçoive les évents du niveau
+		GetPlayerActions().level.connect (GetPlayerActions());
+		// Pour recevoir les évents du joueur
+		GetPlayerEventListener().connect(this);
+		// Pour recevoir la réussite et les echec du joueurs
+		GetPlayerActions().Connect (this);
+
+		// COmmencer à lire la musique
+		GetBeatCounter().StartCount();
+	}
+
+	public void OnFinger(int type) {
+		GetPlayerActions().IsGood(type);
+	}
+
+	public void OnFailure() {
+
+	}
+
+	public void OnSuccess() {
+
+	}
+
+	public void OnSuccessLoop() {
+		successLoopCounter.AddSuccess();
+		successLoop.Play ();
+		if (successLoopCounter.AllSuccess()) {
+			this.StopStep();
+		}
+	}
+
+	private void StopStep() {
+		GetBeatCounter().getMusic ().PauseMusic();
+		endStep();
+		successStep.Play();
+		successLoopCounter.Hide();
+	}
+
+
+}
