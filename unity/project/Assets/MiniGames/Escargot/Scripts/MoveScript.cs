6c460a0e0c66e3a9f6311f894ceeacbdb4476f71
unknown <znoraka@gmail.com>
2015-05-06 13:31:16 +0200

    deplacement par clic escargot

---
+++ unity/project/Assets/MiniGames/Escargot/Scripts/MoveScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	public Parallax [] parallax;
+	public AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void avancer(int n = 5) {
+		foreach (Parallax p in parallax) {
+			p.Move(n);
+		}
+		sound.Play ();
+	}
+}
