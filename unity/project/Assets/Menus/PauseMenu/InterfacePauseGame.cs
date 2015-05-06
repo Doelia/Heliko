a9370832f6b6583f9502ec0c1090646f211d3ff8
robinDesBits <jolan.konig@gmail.com>
2015-05-06 18:26:42 +0200

    Boutique et pause avec retour

--- unity/project/Assets/Menus/PauseMenu/InterfacePauseGame.cs
+++ unity/project/Assets/Menus/PauseMenu/InterfacePauseGame.cs
@@ -11,25 +11,53 @@ public class InterfacePauseGame : HelikoObject {
 	public AudioSource music;
 	public AudioSource openMenuSound;
 	public AudioSource closeMenuSound;
-
+	
+	public bool pause;
 	public new void Start() {
 		base.Start();
 		music = GetBeatCounter().getMusic().GetComponent<AudioSource>();
 	}
 
 	public void UnPause() {
+		pause=false;
 		closeMenuSound.Play();
 		pauseMenu.SetActive(false);
 		music.Play();
 		playIcon.SetActive(false);
 		pauseIcon.SetActive(true);
 	}
-
+	
 	public void Pause() {
+		pause=true;
 		openMenuSound.Play();
 		music.Pause ();
 		pauseMenu.SetActive(true);
 		playIcon.SetActive(true);
 		pauseIcon.SetActive(false);
+	}
+	
+	public void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			if(!pause)
+			{
+				pause=true;
+				openMenuSound.Play();
+				music.Pause ();
+				pauseMenu.SetActive(true);
+				playIcon.SetActive(true);
+				pauseIcon.SetActive(false);
+			}
+			else
+			{
+				pause=false;
+				closeMenuSound.Play();
+				pauseMenu.SetActive(false);
+				music.Play();
+				playIcon.SetActive(false);
+				pauseIcon.SetActive(true);
+			}
+		}
 	}
 }
