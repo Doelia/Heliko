9dd57eaf087fc7ce3ac0aa4dd237e6fdd17d5cfb
robinDesBits <jolan.konig@gmail.com>
2015-05-11 10:09:08 +0200

    Ajout GA pour credits

---
+++ unity/project/Assets/Menus/MiniGamesMenu/ButtonCredit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonCredit : MonoBehaviour {
+
+			public GameObject credits; 
+			public GoogleAnalyticsV3 googleAnalytics;
+
+		
+		public void click() {
+					googleAnalytics.LogEvent(new EventHitBuilder()
+		.SetEventCategory("Button")
+		.SetEventLabel("credit")
+		.SetEventAction("click"));
+		credits.SetActive(true);
+		this.transform.parent.parent.gameObject.SetActive(false);
+	}
+}
