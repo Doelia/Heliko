9c0a24715a647567ef50e877860d98f6848affff
noe <noe.lephilippe@gmail.com>
2015-05-25 00:04:44 +0200

    ajout de commentaires

--- unity/project/Assets/Engine/Timer.cs
+++ unity/project/Assets/Engine/Timer.cs
@@ -3,111 +3,116 @@ using System.Collections;
 
 public abstract class Timer : HelikoObject {
 
-	public bool startCountAtLoad = true;
-	protected bool stopIt = false;
-	private bool loop;
+    public bool startCountAtLoad = true;
+    protected bool stopIt = false;
+    private bool loop;
 
-	public AudioSource audioSource;
-	public float loopTime = 30f; // Temps d'attente entre chaque boucle en MS
+    public AudioSource audioSource;
+    public float loopTime = 30f; // Temps d'attente entre chaque boucle en MS
 
-	private float nextBeatSample; // Le numéro du prochain sample à attendre pour un nouveau beat
-	protected float samplePeriod; // Le temps en samples d'un beat
-	protected float sampleDelay; // Le temps d'attente en sample avant de compter le premier beat
+    private float nextBeatSample; // Le numéro du prochain sample à attendre pour un nouveau beat
+    protected float samplePeriod; // Le temps en samples d'un beat
+    protected float sampleDelay; // Le temps d'attente en sample avant de compter le premier beat
 
-	protected MusicTempo music;
+    protected MusicTempo music;
 
-	private int myMsDelayStartCount = 0;
-	private int myDelayTicks = 0;
-	private int nBeat = 0;
+    private int myMsDelayStartCount = 0;
+    private int myDelayTicks = 0;
+    private int nBeat = 0;
+    
+    //methode à implémenter, elle décrira le comportement lors d'un beat
+    protected abstract void beat();
+    //methode à implémenter, elle décrira le comportement à la fin de la musique
+    protected abstract void endMusic();
 
-	protected abstract void beat();
-	protected abstract void endMusic();
+    protected void setSampleDelay(int msDelayStartCount, int delayTicks) {
+	myMsDelayStartCount = msDelayStartCount;
+	myDelayTicks = delayTicks;
+    }
 
-	protected void setSampleDelay(int msDelayStartCount, int delayTicks) {
-		myMsDelayStartCount = msDelayStartCount;
-		myDelayTicks = delayTicks;
-	}
+    public MusicTempo getMusic() {
+	return music;
+    }
+    
+    //retourne le compte de beat
+    public int getNBeat() {
+	return this.nBeat;
+    }
 
-	public MusicTempo getMusic() {
-		return music;
-	}
+    public new void Start() {
+	if (isStart) return;
 
-	public int getNBeat() {
-		return this.nBeat;
-	}
-
-	public new void Start() {
-		if (isStart) return;
-
-		base.Start();
-
-		if (constantes.instantCalcul)
-			loopTime = 0;
+	base.Start();
 		
-		music = audioSource.GetComponent<MusicTempo>();
+	if (constantes.instantCalcul)
+	    loopTime = 0;
+		
+	music = audioSource.GetComponent<MusicTempo>();
 
-		if (music == null) {
-			Debug.LogError ("MusicTempo component not found");
-			return;
+	if (music == null) {
+	    Debug.LogError ("MusicTempo component not found");
+	    return;
+	}
+
+	float delayMS = myMsDelayStartCount + (myDelayTicks*music.GetTimePeriod()*1000f);
+
+	sampleDelay = ((float) delayMS / 1000f) * music.GetFrequency();
+	samplePeriod = music.GetSamplePeriod();
+	nextBeatSample = sampleDelay;
+
+	if (startCountAtLoad) {
+	    StartCount();
+	} 
+    }
+    
+    //lance le beater
+    public void StartCount() {
+	audioSource.Play();
+	StartCoroutine(BeatCheck());
+    }
+
+    // Stop complétement le thread de comptage
+    public void stopCount() {
+	this.stopIt = true;
+    }
+    
+    //remet la musique au début en réinitialisant toutes les valeurs
+    public void reset() {
+	Debug.Log("reset!");
+	audioSource.Stop();
+	audioSource.Play();
+	nBeat = 0;
+
+	float delayMS = myMsDelayStartCount;
+	sampleDelay = ((float) delayMS / 1000f) * music.GetFrequency();
+	samplePeriod = music.GetSamplePeriod();
+	nextBeatSample = sampleDelay;
+    }
+    
+    //active ou desactive le bouclage de la musique
+    public void setLoop(bool loop) {
+	this.loop = loop;
+    }
+    
+    //thread principal qui se chargera d'envoyer les beats au bon moment
+    IEnumerator BeatCheck () {
+	while (!stopIt) {
+	    if (audioSource.isPlaying) {
+		float currentSample = audioSource.timeSamples;
+		if (currentSample >= (nextBeatSample)) {
+		    this.beat();
+		    nBeat++;
+		    nextBeatSample += samplePeriod;
 		}
-
-		//Debug.Log("Time between ticks : "+music.getTimePeriod()*1000);
-
-		float delayMS = myMsDelayStartCount + (myDelayTicks*music.GetTimePeriod()*1000f);
-
-		sampleDelay = ((float) delayMS / 1000f) * music.GetFrequency();
-		samplePeriod = music.GetSamplePeriod();
-		nextBeatSample = sampleDelay;
-
-		if (startCountAtLoad) {
-			StartCount();
-		} 
-	}
-
-	public void StartCount() {
-		audioSource.Play();
-		StartCoroutine(BeatCheck());
-	}
-
-	// Stop complétement le thread de comptage
-	public void stopCount() {
-		this.stopIt = true;
-	}
-
-	public void reset() {
-		Debug.Log("reset!");
-		audioSource.Stop();
-		audioSource.Play();
-		nBeat = 0;
-
-		float delayMS = myMsDelayStartCount;
-		sampleDelay = ((float) delayMS / 1000f) * music.GetFrequency();
-		samplePeriod = music.GetSamplePeriod();
-		nextBeatSample = sampleDelay;
-	}
-
-	public void setLoop(bool loop) {
-		this.loop = loop;
-	}
-
- 	IEnumerator BeatCheck () {
-		while (!stopIt) {
-			if (audioSource.isPlaying) {
-				float currentSample = audioSource.timeSamples;
-				if (currentSample >= (nextBeatSample)) {
-					this.beat();
-					nBeat++;
-					nextBeatSample += samplePeriod;
-				}
-			}
-			if (audioSource.timeSamples + 1000 >= audioSource.clip.samples) {
-				if(loop) {
-					reset();
-				} else {
-					this.endMusic();
-				}
-			}
-			yield return new WaitForSeconds(loopTime / 1000f);
+	    }
+	    if (audioSource.timeSamples + 1000 >= audioSource.clip.samples) {
+		if(loop) {
+		    reset();
+		} else {
+		    this.endMusic();
 		}
+	    }
+	    yield return new WaitForSeconds(loopTime / 1000f);
 	}
+    }
 }
