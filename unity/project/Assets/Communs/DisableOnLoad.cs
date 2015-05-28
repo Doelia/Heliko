f3b873a3a9b44396d8da57fabfe3ea4db43142e5
Doelia <kyxalia@gmail.com>
2015-05-28 20:02:26 +0200

    Changement accueil - Complètement buggy

---
+++ unity/project/Assets/Communs/DisableOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnLoad : MonoBehaviour {
+
+	void Start () {
+		this.gameObject.SetActive(false);
+	}
+	
+	void Update () {
+	
+	}
+}
